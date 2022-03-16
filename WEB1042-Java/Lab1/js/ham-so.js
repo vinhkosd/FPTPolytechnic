d5449352f60148739eea3f5d7e04747f5206af23
vinhkosd <vinh8c059@gmail.com>
2022-03-16 13:39:08 +0700

    add Lab2 COM2012, Lab1-2 WEB1042

---
+++ WEB1042-Java/Lab1/js/ham-so.js
@@ -0,0 +1,8 @@
+function kiem_tra(a) {
+    if (a % 2 == 0) {
+        alert(a + " là số chẵn");
+    }
+    else {
+        alert(a + " là số lẻ");
+    }
+}
---
+++ WEB1042-Java/Lab1/js/hello2.js
@@ -0,0 +1,2 @@
+alert("Welcome to FPT Polytechnic");
+document.write("<h1>The first JavaScript App</h1>");
